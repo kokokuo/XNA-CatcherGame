ebc8189621d102fb3aa84c303225ced990adc5a3
yi-cheng-kuo <v6610688@gmail.com>
2013-10-29 13:11:04 +0800

    10-29 People類別動畫撥放修改,AnimationScripte加入判斷是否播完一輪的設定

--- CatcherGame/CatcherGame/GameObjects/People.cs
+++ CatcherGame/CatcherGame/GameObjects/People.cs
@@ -18,7 +18,7 @@ namespace CatcherGame.GameObjects
         List<AnimationSprite> animationList;
         AnimationSprite pCurrentAnimation;
         bool isFalling, isCaught, isSaved;
-        
+        const int FALLING_KEY = 0, CAUGHT_KEY = 1, WALK_KEY = 2;
         public People(GameState currentGameState, int id, float x, float y, float fallingSpeed, float fallingWave)
             : base(currentGameState, id, x, y) 
         { 
@@ -48,27 +48,38 @@ namespace CatcherGame.GameObjects
         public void SetCaught() {
             isCaught = true;
             isFalling = false;
+            //切換圖片組
+            pCurrentAnimation = animationList[CAUGHT_KEY];
         }
         public override void LoadResource(TexturesKeyEnum key)
         {
             SetTexture2DList(key);
+            //設定Caught圖片組的第二張圖片延遲一秒 (從接住到站起來這段時間)
+            //caught圖片組第一張是接住(Index =0),第二張是站起來,對接住的圖片delay 1秒(1000ms)
+            animationList[CAUGHT_KEY].SetCertainFrameDelayTime(0, 1000);
+
+            //設定目前的圖片組是掉下來
+            pCurrentAnimation = animationList[FALLING_KEY];
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            pCurrentAnimation.Draw(spriteBatch);
         }
 
         public override void Update()
         {
             if (isFalling) {
-                pCurrentAnimation = animationList[0];
+                //判斷有無落到地板 
             }
             else if (isCaught) { 
-                pCurrentAnimation = animationList[1];
-                
+               
+                if(pCurrentAnimation.GetCurrentFrameNumber == (pCurrentAnimation.GetFrameSize -1 )){
+                    isCaught = false;
+                    
+                }
             }
-           
+            else if(isSaved)
         }
     }
 }
